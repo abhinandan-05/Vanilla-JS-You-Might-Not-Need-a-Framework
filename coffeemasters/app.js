03b5d53edf615f398f316284a34c9fa346931614
Abhinandan Kumar <abhicpr05@gmail.com>
2025-06-12 11:46:45 +0530

    router

--- coffeemasters/app.js
+++ coffeemasters/app.js
@@ -1,12 +1,15 @@
 import Store from "../coffeemasters/services/Store.js";
 import API from "../coffeemasters/services/API.js";
 import { loadData } from "../coffeemasters/services/Menu.js";
+import Router from "../coffeemasters/services/Router.js";
 
 // making Store modue accessible to globle
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 // It's better to wait for the event for manipulation
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
+  app.router.init();
 });
